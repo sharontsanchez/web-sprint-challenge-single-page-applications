e01510edc0ae6d996f90664e87329bdd3133afd9
Sharon Sanchez <sstharakan@gmail.com>
2021-11-12 11:20:51 -0600

    setting up useState, routes, link and switch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import Home from "./Home";
+import OrderForm from "./OrderForm";
 
+
+export const toppings = ["meat lovers", "veggie lovers", "cheese lovers", "pineapple"]
 const App = () => {
+  // initial values 
+  const initialFormValues = {
+    name: "",
+    size: "",
+    specialInstructions: "",
+  };
+  const initialErrorValues = { ...initialFormValues};
+// include toppings in initial values 
+  toppings.forEach((topping) => (initialFormValues[topping] = false));
+  toppings.forEach((topping) => (initialErrorValues[topping] = ""));
+  // Set up States 
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [errorValues, setErrorValues] = useState(initialErrorValues);
   return (
     <>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/pizza">Pizza</Link>
+      </header>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <Switch>
+        <Route path="/">
+          <Home/>
+         </Route>
+        <Route path="/pizza">
+          <OrderForm/>
+        </Route>
+      </Switch>
     </>
   );
 };
